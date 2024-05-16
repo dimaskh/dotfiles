5d03bc644a172d448c9feb823687be9ad9fb8670
dimaskh <dimaskh.dev@gmail.com>
2024-05-16 20:42:57 +0300

    Start awesome from scratch

--- .config/awesome.bak/signals/custom/init.lua
+++
@@ -1,9 +0,0 @@
-local awful = require'awful'
-
--- Run authentication agent on startup
-awesome.connect_signal(
-    'startup',
-    function(args)
-        awful.util.spawn('/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1')
-    end
-)
--- .config/awesome.bak/signals/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-  client = require'signals.client',
-  custom = require'signals.custom',
-  naughty = require'signals.naughty',
-  ruled = require'signals.ruled',
-  screen = require'signals.screen',
-  tag = require'signals.tag',
-}
